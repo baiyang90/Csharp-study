c283f413a3644d0aba2c7cf91821f6f5fa711294
by <yiyi42582@gmail.com>
2023-05-16 13:39:40 +0800

    123

--- Program.cs
+++ Program.cs
@@ -1,43 +1,35 @@
-﻿namespace ConsoleApp1
+﻿using System.Net.Http;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
 
-        delegate void Hellowde(string msg);
-        static void Main(string[] args)
+        
+        static async Task Main(string[] args)
         {
 
-            List<LearnVip> listlearnvip = new()
-            {
-                new LearnVip() { Id = 1, Name = "a1", price = 444 },
-                new LearnVip() { Id = 2, Name = "a2", price = 555 },
-                new LearnVip() { Id = 3, Name = "a3", price = 666 },
-                new LearnVip() { Id = 4, Name = "a4", price = 777 },
-                new LearnVip() { Id = 5, Name = "a5", price = 888 }
-            };
-
-
-            listlearnvip.Where(x => x.price >= 600).ToList().ForEach(x => Console.WriteLine(x.Name + " 是VIP"));
+            //Console.WriteLine("ssss");
+            //await Testasync();
+            //Console.WriteLine("bbbbb");
+            await Gethttpstring(@"http://www.baidu.com", @"c:\by\baidu.txt");
+            Console.WriteLine("123");
         }
 
-
-
-    }
-
-    class LearnVip
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public int price { get; set; }
-        public static void JisuanVip(List<LearnVip> Vips)
+        public static Task Testasync()
         {
-            foreach (var vip in Vips)
+            Thread.Sleep(1000);
+            Console.WriteLine("这是等待1s得方法");
+            return Task.CompletedTask;
+        }
+        public static async Task Gethttpstring(string url, string filename)
             {
-                if (vip.price >= 600)
-                {
-                    Console.WriteLine(vip.Name + " 是VIP");
-                }
+            HttpClientFactory
+            HttpClient client = new HttpClient();
+            string s = await client.GetStringAsync(url);
+            await File.WriteAllTextAsync(filename, s);
             }
-        }
     }
+
+
 }
